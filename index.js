ca002305541cba2081adf0aedca8260b8dfc18ba
Anaxirinia Jimenez <anax131@gmail.com>
2017-12-16 19:54:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,3 +11,4 @@ var newID = 1000000000 + oldID;
 var ageIsValid = Number.isInteger(currentAge);
 var randomNumber = Math.floor(Math.random() * 20) ;
 var randomInteger = Math.floor(randomNumber);
+var randomUserID = randomNumber + 1000000000;
